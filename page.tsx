d71391b27c40f1bcfb3cd3815523e5040791d0b2
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-13 03:12:19 -0500

    Delete page.tsx

--- page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1 style={{ margin: 0 }}>SSGPT6 Trade Hub</h1>
-      <p style={{ marginTop: 8 }}>Trade Hub is live. ✅</p>
-    </main>
-  );
-}
