97cfe52ed39e7e0279b5d0d146a6e9686d620be9
shanta4100 <p888qd7jc8@privaterelay.appleid.com>
2026-02-14 09:40:27 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>SS GPT6 Trade Hub</h1>
+      <p>Deployment Active</p>
+    </main>
+  );
+}
